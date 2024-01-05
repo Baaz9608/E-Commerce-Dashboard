fc23f5a8df9276444d42bf441f3f69421cee825e
BaazShah606 <shahbazmd3002@gmail.com>
2024-01-05 08:10:57 +0530

    revision for connection of data with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+const connectDB = async()=>{
+    mongoose.connect('mongodb://localhost:27017/e-comm');
+    const productSchema = new mongoose.Schema({
+        name: String,
+        email: String
+    })
+    const product = mongoose.model('product',productSchema);
+    const data = await product.find();
+    console.log(data);
+    
+}
+connectDB();
+
 
 app.get('/', (req,res)=>{
     res.send('done')
