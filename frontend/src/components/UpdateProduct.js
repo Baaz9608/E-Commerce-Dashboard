b066e269570bd3ddf8c04a120e26d390ee604657
BaazShah606 <shahbazmd3002@gmail.com>
2024-01-09 02:22:16 +0530

    updated

--- frontend/src/components/UpdateProduct.js
+++ frontend/src/components/UpdateProduct.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
  
@@ -8,6 +9,23 @@ const UpdateProduct = ()=>{
     const [price, setPrice] = useState('');
     const [category, setCategory] = useState('');
     const [company, setCompany] = useState('');
+    const params = useParams();
+
+    useEffect(()=>{
+        // console.log(params.id)
+        getProductDetails();
+    },[])
+
+    const getProductDetails = async()=>{
+        console.log(params);
+        let result = await fetch(`http://127.0.0.1:5000/product/${params.id}`)
+        result = await result.json();
+        console.log(result);
+        setName(result.name);
+        setPrice(result.price);
+        setCategory(result.category);
+        setCompany(result.company);
+    }
 
     const updateProduct = ()=>{
         console.log(name, price, category, company);
